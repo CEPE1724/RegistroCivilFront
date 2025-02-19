69396b09d84890b89d2ef6389ac989724e371f83
Isabela Tejada <isabelatejada@IsisMacBookPro.local>
2025-02-19 15:22:09 -0500

    feat(login):Terminado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,8 +13,9 @@ import Ponty from "../img/ponty.png";
 import axios from "../configApi/axiosConfig";
 import { useSnackbar } from "notistack";
 import PersonIcon from "@mui/icons-material/Person";
+import { Box, Typography } from "@mui/material";
 const Login = () => {
-  const Logo = "/img/logo.webp"; // Ruta relativa desde la carpeta public
+  const Logo = "/img/Point.png"; // Ruta relativa desde la carpeta public
 
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
@@ -23,13 +24,12 @@ const Login = () => {
   const { enqueueSnackbar } = useSnackbar();
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     if (localStorage.getItem("token")) {
       navigate("/nueva-consulta", { replace: true });
     }
   }, [navigate]);
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -95,24 +95,55 @@ const Login = () => {
 
   return (
     <>
-      <section className=" flex items-center justify-center min-h-screen bg-red-600" style={{ background: "linear-gradient(to bottom, #1965F2, #D9E6FF)" }} >
+      <section
+        className=" flex items-center justify-center min-h-screen bg-red-600"
+        style={{ background: "linear-gradient(to bottom, #1965F2, #D9E6FF)" }}
+      >
         <div className="container max-w-4xl p-4 sm:p-6 md:p-10">
           <div className="flex flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-            <div className="w-full">
-              <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-                <div className="flex flex-col lg:flex-row">
+            <div className="w-full " style={{borderRadius: "20px"}}>
+              <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800" style={{borderRadius: "20px"}}>
+                <div
+                  className="flex flex-col lg:flex-row relative rounded-lg "
+                  style={{
+                    background:
+                      "linear-gradient(90deg, #5592F3 0%, #A6C8FF 50%, #ffffff 100%)",
+                      borderRadius: "20px",
+                  }}
+                >
                   {/* Formulario de inicio de sesión */}
-                  <div className="w-full lg:w-6/12 px-4 py-8 md:px-6 bg-gradient-to-r from-[#5592F3] to-[#ffffff]">
+                  <div className="w-full lg:w-6/12 px-4 py-8 md:px-6 relative">
+                     {   /* Capa morada semitransparente */}
+                    <div
+                      className="absolute inset-0 z-10"
+                      style={{
+                        background: `linear-gradient(
+                           to bottom,
+                                    rgb(16, 16, 18) 100%,
+                                 rgba(10, 9, 16, 0.6) 61%,
+                                 rgba(171, 163, 236, 0.5) 61%,
+                                  rgba(0, 0, 0, 0) 100%
+                                  )`,
+                        mixBlendMode: "overlay",
+                        borderTopLeftRadius: "20px",
+                        borderBottomLeftRadius: "20px",
+                        
+                      }}
+                    ></div>
+
                     <img
-                      className="mx-auto w-[80px] sm:w-[100px] md:w-[120px] lg:w-2/3"
+                      className="mx-auto w-[80px] sm:w-[100px] md:w-[120px] lg:w-2/3 mb-6 pb-2"
                       src={Logo}
                       alt="logo"
                     />
-                    <div className="md:mx-6 p-8 bg-slate-50 rounded-2xl">
+                    <div
+                      className="md:mx-6 p-8 rounded-2xl"
+                      style={{ backgroundColor: "rgba(255, 255, 255, 0.50)" }}
+                    >
                       <div className="text-center">
                         {/* <h4 className="text-2xl font-semibold mb-6">
-                          Registro Ciudadanos
-                        </h4> */}
+                                Registro Ciudadanos
+                              </h4> */}
                         <h5 className="pt-4 pb-6 text-xl font-semibold">
                           Iniciar Sesión
                         </h5>
@@ -123,113 +154,165 @@ const Login = () => {
                         </div>
                       )}
                       <form className="space-y-4" onSubmit={handleSubmit}>
-                        <TextField
-                          label="Usuario POINT"
-                          fullWidth
-                          required
-                          value={userName}
-                          variant="outlined"
-                          sx={{
-                            "& .MuiOutlinedInput-root": {
-                              "&.Mui-focused fieldset": {
-                                borderColor: "#2d3689",
+                        <Box>
+                          <Typography
+                            sx={{
+                              fontWeight: "bold",
+                              fontSize: "0.95rem",
+                              color: "#000",
+                              mb: 0.5,
+                            }}
+                          >
+                            Usuario POINT 
+                          </Typography>
+
+                          <TextField
+                          placeholder="username@gmail.com"
+                            fullWidth
+                            required
+                            value={userName}
+                            variant="outlined"
+                            sx={{
+                              backgroundColor: "white",
+                              borderRadius: "4px",
+                              "& .MuiOutlinedInput-root": {
+                                "& fieldset": {
+                                  borderColor: "transparent", // Sin borde
+                                },
+                                "&:hover fieldset": {
+                                  borderColor: "#ccc", // Color al pasar el mouse
+                                },
+                                "&.Mui-focused fieldset": {
+                                  borderColor: "#2d3689", // Color al enfocar
+                                },
                               },
-                            },
-                            "& .MuiInputLabel-root.Mui-focused": {
-                              color: "#2d3689",
-                            },
-                          }}
-                          onChange={(e) =>
-                            setUserName(e.target.value.toUpperCase())
-                          } // Convertir a mayúsculas
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <PersonIcon sx={{ fontSize: "1.3rem" }} />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                        <TextField
-                          label="Contraseña"
-                          variant="outlined"
-                          sx={{
-                            "& .MuiOutlinedInput-root": {
-                              "&.Mui-focused fieldset": {
-                                borderColor: "#2d3689",
+                              "& input": {
+                                padding: "12px 14px",
+                                paddingLeft: "0px",  // Ajusta el padding interno
                               },
-                            },
-                            "& .MuiInputLabel-root.Mui-focused": {
-                              color: "#2d3689",
-                            },
-                          }}
-                          type={showPassword ? "text" : "password"} // Cambia entre texto y contraseña
-                          fullWidth
-                          required
-                          value={password}
-                          onChange={(e) =>
-                            setPassword(e.target.value.toUpperCase())
-                          } // Convertir a mayúsculas
-                          InputProps={{
-                            startAdornment: (
+                            }}
+                            onChange={(e) =>
+                              setUserName(e.target.value.toUpperCase())
+                            } // Convertir a mayúsculas
+                            InputProps={{
+                              startAdornment: (
+                                <InputAdornment position="start">
+                                  <PersonIcon
+                                    sx={{
+                                      fontSize: "1.3rem",
+                                      color: "#000",
+                                    }}
+                                  />
+                                </InputAdornment>
+                              ),
+                            }}
+                          />
+                        </Box>
+
+                        <Box sx={{ widht: "100%" }}>
+                          <Typography
+                            sx={{
+                              fontWeight: "bold",
+                              fontSize: "0.95rem",
+                              color: "#000",
+                              mb: 0.5,
+                            }}
+                          >
+                            Contraseña
+                          </Typography>
+
+                          <TextField
+                            fullWidth
+                            required
+                            value={password}
+                            variant="outlined"
+                            sx={{
+                              backgroundColor: "white",
+                              borderRadius: "4px",
+                              "& .MuiOutlinedInput-root": {
+                                "& fieldset": {
+                                  borderColor: "transparent", // Sin borde
+                                },
+                                "&:hover fieldset": {
+                                  borderColor: "#ccc", // Color al pasar el mouse
+                                },
+                                "&.Mui-focused fieldset": {
+                                  borderColor: "#2d3689", // Color al enfocar
+                                },
+                              },
+                              "& input": {
+                                padding: "12px 14px", // Ajusta el padding interno
+                              },
+                            }}
+                            onChange={(e) =>
+                              setPassword(e.target.value.toUpperCase())
+                            } // Convertir a mayúsculas
+                            type={showPassword ? "text" : "password"} // Cambia entre texto y contraseña
+                            InputProps={{
+                              startAdornment: (
                               <InputAdornment position="start">
-                                <Lock sx={{ fontSize: "1.3rem" }}/>
+                                <Lock
+                                sx={{
+                                  fontSize: "1.3rem",
+                                  color: "#000",
+                                }}
+                                />
                               </InputAdornment>
-                            ),
-                            endAdornment: (
+                              ),
+                              endAdornment: (
                               <InputAdornment position="end">
                                 <IconButton
-                                  onClick={handleTogglePasswordVisibility}
-                                  edge="end"
+                                onClick={handleTogglePasswordVisibility}
+                                edge="end"
                                 >
-                                  {showPassword ? (
-                                    <VisibilityOff
-                                      sx={{ fontSize: "1.3rem" }}
-                                    />
-                                  ) : (
-                                    <Visibility sx={{ fontSize: "1.3rem" }} />
-                                  )}
+                                {showPassword ? (
+                                  <VisibilityOff
+                                  sx={{
+                                    fontSize: "1.3rem",
+                                    color: "#000",
+                                  }}
+                                  />
+                                ) : (
+                                  <Visibility
+                                  sx={{
+                                    fontSize: "1.3rem",
+                                    color: "#000",
+                                  }}
+                                  />
+                                )}
                                 </IconButton>
                               </InputAdornment>
-                            ),
-                          }}
-                        />
-                        <button
-                          className="mb-2 w-full bg-morado text-white py-2 px-4 rounded-md hover:bg-moradoHover flex items-center justify-center"
-                          type="submit"
-                        >
-                          {isLoading ? (
+                              ),
+                            }}
+                            />
+                          </Box>
+
+                          <button
+                            className="mb-2 w-full text-white py-2 px-4 rounded-md hover:bg-moradoHover flex items-center justify-center" style={{backgroundColor: "#1965F2"}}
+                            type="submit"
+                          >
+                            {isLoading ? (
                             <CircularProgress size={24} color="inherit" />
-                          ) : (
+                            ) : (
                             <span className="flex items-center">
                               Ingresar <LoginIcon className="ml-2" />
                             </span>
-                          )}
-                        </button>
-                      </form>
-                    </div>
-                  </div>
+                            )}
+                          </button>
+                          </form>
+                        </div>
+                        </div>
 
-                  {/* Sección de imágenes y texto informativo */}
-                  <div
-                    className="w-full lg:w-6/12 flex items-center justify-center rounded-b-lg lg:rounded-e-lg lg:rounded-bl-none"
-                    style={{
-                      background:
-                        "linear-gradient(to right, #212863, #3c46a5, #6f79d5, #b5bcfc)",
-                    }}
-                  >
+                        {/* Sección de imágenes y texto informativo */}
+                  <div className="w-full lg:w-6/12 flex items-center justify-start">
+                    <img className="" src={Ponty} alt="logo" />
                     <div className="px-4 py-6 text-white md:mx-6 md:p-12">
                       <div className="mt-8 text-center">
                         {/* <img
-                          className="mx-auto w-[80px] sm:w-[100px] md:w-[120px] lg:w-2/3"
-                          src={Logo}
-                          alt="logo"
-                        /> */}
-                        <img
-                          className="mx-auto mt-4 w-[80px] sm:w-[100px] md:w-[120px] lg:w-1/3"
+                          className="w-[1000px] h-[500px]"
                           src={Ponty}
                           alt="logo"
-                        />
+                        />*/}
                         {/* <p className="text-white">
                           BDD de ciudadanos o nueva consulta al Registro Civil.
                         </p> */}
