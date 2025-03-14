7af4da9c5f7364f51fbbda6fd2ae553d3d236e8f
Isabela Tejada <isabelatejada@IsisMacBookPro.mshome.net>
2025-03-14 15:17:51 -0500

    ya consulta de base de datos en documental

--- src/configApi/apiConfig.js
+++ src/configApi/apiConfig.js
@@ -1,7 +1,7 @@
 import { get } from "react-hook-form";
 
 
-const API_BASE_URL = "http://192.168.2.167:3008/api/v1/";
+const API_BASE_URL = "http://192.168.137.28:3025/api/v1/";
 
 const SOCKET_BASE_URL = "http://192.168.2.167:3008"; 
 //const API_BASE_URL = "https://appservices.com.ec/cobranza/api/v1/point/";
@@ -72,5 +72,7 @@ export const APIURL = {
 	get_cre_tiempoVivienda : () => `${API_BASE_URL}cre-tiempovivienda`,
 
    post_creVerificacionTelefonica : () => `${API_BASE_URL}creverificaciontelefonica`,
+   post_documentos : () => `${API_BASE_URL}documentos-solicitud`,
+   get_documentos : (id) => `${API_BASE_URL}documentos-solicitud/${id}`,
 
 };
