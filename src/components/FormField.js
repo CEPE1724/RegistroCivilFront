89a29f98d361f53f81a0f3c072aa24e4027b88f4
Duard <emoya@point.com.ec>
2025-03-19 16:28:45 -0500

    loader agregado a solicitud

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -5,6 +5,7 @@ import OTPModal from '../components/SolicitudCredito/SolicitudCreditoForm/OTPMod
 import { APIURL } from "../configApi/apiConfig";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Loader } from "../components/Utils/Loader";
 
 const FormField = ({
   label,
@@ -314,16 +315,19 @@ const ReusableForm = ({
 //     setIsOtpModalOpen(false); // Cerrar el modal después de la verificación del OTP
 //   };
 
-const handleOtpVerification = async (isVerified) => {
+const handleOtpVerification = async (isVerified, otpCode) => {
 	if (isVerified) {
+	  let otpCodeString = String(otpCode);
+	  // Se asigna el OTP al objeto de Formik
+	  formik.values.otp_code = otpCodeString;
 	  setIsSubmitting(true);
 	  try {
 		await onSubmit(formik.values);
-	  
+		// Espera 2 segundos para que el loader se muestre
+		await new Promise((resolve) => setTimeout(resolve, 2000));
 		formik.resetForm();
-    navigate("/ListadoSolicitud", { replace: true });
-
 		setPreviewUrl(null);
+		navigate("/ListadoSolicitud", { replace: true });
 	  } catch (error) {
 		enqueueSnackbar("Error al enviar el formulario", { variant: "error" });
 		console.error("Error en envío:", error);
@@ -334,154 +338,165 @@ const handleOtpVerification = async (isVerified) => {
 	setIsOtpModalOpen(false);
 	setIsOtpVerified(isVerified);
   };
+  
+  
 
   // Separar el campo de archivo y otros campos
   const fileField = formConfig.find((field) => field.type === "file");
   const otherFields = formConfig.filter((field) => field.type !== "file");
 
   return (
-    <form
-      className="w-full bg-white p-4 rounded-lg grid"
-      onSubmit={(e) => {
-        e.preventDefault();
-        // Validar y mostrar el primer error si hay alguno
-        formik.validateForm().then(errors => {
-          if (Object.keys(errors).length > 0) {
-			formik.setTouched(
+	<div className="relative">
+	  {isSubmitting && (
+		<div className="absolute inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75">
+		  <Loader />
+		</div>
+	  )}
+	  <form
+		className="w-full bg-white p-4 rounded-lg grid"
+		onSubmit={(e) => {
+		  e.preventDefault();
+		  // Validar y mostrar el primer error si hay alguno
+		  formik.validateForm().then((errors) => {
+			if (Object.keys(errors).length > 0) {
+			  formik.setTouched(
 				Object.keys(errors).reduce((acc, key) => ({ ...acc, [key]: true }), {}),
 				false
 			  );
-            showFirstError();
-          } else {
-            formik.handleSubmit(e);
-          }
-        });
-      }}
-    >
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
-        {fileField && (
-          <div className="md:col-span-1 order-2 md:order-1">
-            <div className="w-30">
-              <FormField
-                key={fileField.name}
-                {...fileField}
-                formik={formik}
-                previewUrl={previewUrl}
-                setPreviewUrl={setPreviewUrl}
-              />
-            </div>
-          </div>
-        )}
-
-        <div
-          className={`grid gap-4 order-1 md:order-2 ${fileField ? "md:col-span-3" : "md:col-span-4"
-            } ${columns === 2
-              ? "grid-cols-1 md:grid-cols-2"
-              : columns === 3
-                ? "grid-cols-1 md:grid-cols-3"
-                : columns === 4
-                  ? "grid-cols-1 md:grid-cols-4"
-                  : "grid-cols-1"
-            }`}
-        >
-          {otherFields.map((field) => (
-            <FormField key={field.name} {...field} formik={formik} />
-          ))}
-        </div>
-      </div>
-
-      {/* Términos y condiciones */}
-      {includeTermsAndConditions && (
-        <div className="flex justify-center gap-4 mt-4 mb-4">
-          <div className="flex items-center gap-2">
-            <input
-              type="checkbox"
-              name="bTerminosYCondiciones"
-              id="bTerminosYCondiciones"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              checked={formik.values.bTerminosYCondiciones}
-              className="text-fontRed accent-fontRed rounded"
-            />
-            <label
-              htmlFor="bTerminosYCondiciones"
-              className="text-lightGrey text-xs"
-            >
-              Acepto los{" "}
-              <a
-                href="/terminos-y-condiciones"
-                className="underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                términos y condiciones
-              </a>
-            </label>
-          </div>
-          {formik.errors.bTerminosYCondiciones &&
-            formik.touched.bTerminosYCondiciones && (
-              <p className="text-red-500 text-xs mt-1 text-center">
-                {formik.errors.bTerminosYCondiciones}
-              </p>
-            )}
-
-          <div className="flex items-center gap-2">
-            <input
-              type="checkbox"
-              name="bPoliticas"
-              id="bPoliticas"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              checked={formik.values.bPoliticas}
-              className="text-fontRed accent-fontRed rounded"
-            />
-            <label htmlFor="bPoliticas" className="text-lightGrey text-xs">
-              Acepto las{" "}
-              <a
-                href="/politicas-de-privacidad"
-                className="underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                políticas de privacidad
-              </a>
-            </label>
-          </div>
-          {formik.errors.bPoliticas && formik.touched.bPoliticas && (
-            <p className="text-red-500 text-xs mt-1 text-center">
-              {formik.errors.bPoliticas}
-            </p>
-          )}
-        </div>
-      )}
-
-      {includeButtons && (
-        <div className="py-2 flex justify-center gap-2">
-          <button
-            type="submit"
-            className="rounded-full hover:shadow-md transition duration-300 ease-in-out group bg-primaryBlue text-white border border-white hover:bg-white hover:text-primaryBlue hover:border-primaryBlue text-xs px-6 py-2.5"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Enviando..." : submitButtonText}
-          </button>
-          <button
-            type="button"
-            className="rounded-full hover:shadow-md transition duration-300 ease-in-out group bg-gray-400 text-white border border-white hover:bg-white hover:text-gray-400 hover:border-gray-400 text-xs px-6 py-2.5"
-            onClick={handleCancel}
-            disabled={isCanceling}
-          >
-            {isCanceling ? "Cancelando..." : cancelButtonText}
-          </button>
-        </div>
-      )}
-      {<OTPModal
-        isOpen={isOtpModalOpen}
-        onClose={() => setIsOtpModalOpen(false)}
-        onVerifyOtp={handleOtpVerification}
-        phoneNumberOTP = {formik.values.Celular}
-      />}
-    </form>
+			  showFirstError();
+			} else {
+			  formik.handleSubmit(e);
+			}
+		  });
+		}}
+	  >
+		<div className="grid gap-4 grid-cols-1 md:grid-cols-4">
+		  {fileField && (
+			<div className="md:col-span-1 order-2 md:order-1">
+			  <div className="w-30">
+				<FormField
+				  key={fileField.name}
+				  {...fileField}
+				  formik={formik}
+				  previewUrl={previewUrl}
+				  setPreviewUrl={setPreviewUrl}
+				/>
+			  </div>
+			</div>
+		  )}
+  
+		  <div
+			className={`grid gap-4 order-1 md:order-2 ${
+			  fileField ? "md:col-span-3" : "md:col-span-4"
+			} ${
+			  columns === 2
+				? "grid-cols-1 md:grid-cols-2"
+				: columns === 3
+				? "grid-cols-1 md:grid-cols-3"
+				: columns === 4
+				? "grid-cols-1 md:grid-cols-4"
+				: "grid-cols-1"
+			}`}
+		  >
+			{otherFields.map((field) => (
+			  <FormField key={field.name} {...field} formik={formik} />
+			))}
+		  </div>
+		</div>
+  
+		{/* Términos y condiciones */}
+		{includeTermsAndConditions && (
+		  <div className="flex justify-center gap-4 mt-4 mb-4">
+			<div className="flex items-center gap-2">
+			  <input
+				type="checkbox"
+				name="bTerminosYCondiciones"
+				id="bTerminosYCondiciones"
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				checked={formik.values.bTerminosYCondiciones}
+				className="text-fontRed accent-fontRed rounded"
+			  />
+			  <label htmlFor="bTerminosYCondiciones" className="text-lightGrey text-xs">
+				Acepto los{" "}
+				<a
+				  href="/terminos-y-condiciones"
+				  className="underline"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				  términos y condiciones
+				</a>
+			  </label>
+			</div>
+			{formik.errors.bTerminosYCondiciones &&
+			  formik.touched.bTerminosYCondiciones && (
+				<p className="text-red-500 text-xs mt-1 text-center">
+				  {formik.errors.bTerminosYCondiciones}
+				</p>
+			  )}
+  
+			<div className="flex items-center gap-2">
+			  <input
+				type="checkbox"
+				name="bPoliticas"
+				id="bPoliticas"
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				checked={formik.values.bPoliticas}
+				className="text-fontRed accent-fontRed rounded"
+			  />
+			  <label htmlFor="bPoliticas" className="text-lightGrey text-xs">
+				Acepto las{" "}
+				<a
+				  href="/politicas-de-privacidad"
+				  className="underline"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				  políticas de privacidad
+				</a>
+			  </label>
+			</div>
+			{formik.errors.bPoliticas && formik.touched.bPoliticas && (
+			  <p className="text-red-500 text-xs mt-1 text-center">
+				{formik.errors.bPoliticas}
+			  </p>
+			)}
+		  </div>
+		)}
+  
+		{includeButtons && (
+		  <div className="py-2 flex justify-center gap-2">
+			<button
+			  type="submit"
+			  className="rounded-full hover:shadow-md transition duration-300 ease-in-out group bg-primaryBlue text-white border border-white hover:bg-white hover:text-primaryBlue hover:border-primaryBlue text-xs px-6 py-2.5"
+			  disabled={isSubmitting}
+			>
+			  {isSubmitting ? <Loader /> : submitButtonText}
+			</button>
+			<button
+			  type="button"
+			  className="rounded-full hover:shadow-md transition duration-300 ease-in-out group bg-gray-400 text-white border border-white hover:bg-white hover:text-gray-400 hover:border-gray-400 text-xs px-6 py-2.5"
+			  onClick={handleCancel}
+			  disabled={isCanceling}
+			>
+			  {isCanceling ? "Cancelando..." : cancelButtonText}
+			</button>
+		  </div>
+		)}
+		{
+		  <OTPModal
+			isOpen={isOtpModalOpen}
+			onClose={() => setIsOtpModalOpen(false)}
+			onVerifyOtp={handleOtpVerification}
+			phoneNumberOTP={formik.values.Celular}
+		  />
+		}
+	  </form>
+	</div>
   );
+  
 };
 
 export default ReusableForm;
