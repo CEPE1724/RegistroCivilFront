b7e704a3a5ec5d253f943a54425ac9d375eb1c6b
Isabela Tejada <isabelatejada@IsisMacBookPro.mshome.net>
2025-03-20 15:40:37 -0500

    dashboard casi terminado falta validar fechas

--- src/components/Dashboards/Dashboards.js
+++ src/components/Dashboards/Dashboards.js
@@ -1,24 +1,108 @@
 import React, { useEffect, useState } from "react";
-import { MonetizationOn, People, ShoppingCart, ConfirmationNumber } from "@mui/icons-material";
+import {
+  MonetizationOn,
+  People,
+  ShoppingCart,
+  ConfirmationNumber,
+} from "@mui/icons-material";
 import { Doughnut, Bar } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
-import { APIURL } from '../../configApi/apiConfig';
+import { APIURL } from "../../configApi/apiConfig";
+import useBodegaUsuario from "../../hooks/useBodegaUsuario";
+import { useAuth } from "../AuthContext/AuthContext";
+
 import axios from "axios";
 
 Chart.register(...registerables);
 
 export function Dashboards() {
+
+  const estadosOpciones = [
+    { label: "Todos", value: "todos" },
+    { label: "Pendiente", value: 1 },
+    { label: "Aprobado", value: 2 },
+    { label: "Anulado", value: 3 },
+    { label: "Rechazado", value: 4 },
+  ];
+  const [estadoFiltro, setEstadoFiltro] = useState("todos");
+
+
+
+  const isValidDate = (dateString) => {
+    const regex = /^\d{4}-\d{2}-\d{2}$/; // Expresión regular para el formato YYYY-MM-DD
+    if (!regex.test(dateString)) return false; // Si el formato no coincide, es inválido
+
+    const date = new Date(dateString);
+    return !isNaN(date.getTime()); // Verifica si la fecha es válida
+  };
+
+const [uniqueVendedores, setUniqueVendedores] = useState(0);
+
+  const today = new Date().toISOString().split("T")[0]; // Obtener la fecha de hoy en formato YYYY-MM-DD
   const [doughnutData, setDoughnutData] = useState({
     labels: ["Pendiente", "Aprobado", "Anulado", "Rechazado"],
-    datasets: [{ data: [0, 0, 0, 0], backgroundColor: ["#007bff", "#00c853", "#ffab00", "#ff3d00"] }],
+    datasets: [
+      {
+        data: [0, 0, 0, 0],
+        backgroundColor: ["#007bff", "#00c853", "#ffab00", "#ff3d00"],
+      },
+    ],
   });
 
+  const [barData, setBarData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Solicitudes por Bodega",
+        data: [],
+        backgroundColor: "#007bff",
+      },
+    ],
+  });
   const [totalSolicitudes, setTotalSolicitudes] = useState(0);
-  const [fechaInicio, setFechaInicio] = useState("");
-  const [fechaFin, setFechaFin] = useState("");
+  const [fechaInicio, setFechaInicio] = useState(today);
+  const [fechaFin, setFechaFin] = useState(today);
+  const [selectedBodega, setSelectedBodega] = useState("todos");
+  const { data, loading, error, fetchBodegaUsuario } = useBodegaUsuario();
+  const { userData } = useAuth();
+  console.log("EDIOSN", userData);
+  const [bodegass, setBodegass] = useState([]);
 
-  const fetchSolicitudes = async (fechaInicio, fechaFin) => {
+  const bodegas = data || [];  // Safely access the bodegas data
+
+
+  useEffect(() => {
+    if (userData && userData.idUsuario) {
+    fetchBodega(); // Llamar a la API para obtener las bodegas
+    fetchSolicitudes();
+    } // Llamar a la API para obtener las solicitudes
+  }, [userData]);
+
+  const handleBodegaChange = (event) => {
+    setSelectedBodega(event.target.value);
+  };
+
+  const fetchBodega = async () => {
+    const userId = userData.idUsuario;
+    const idTipoFactura = 43;
+    const fecha = new Date().toISOString();
+    const recibeConsignacion = true;
+
     try {
+      await fetchBodegaUsuario(userId, idTipoFactura, fecha, recibeConsignacion);
+      console.log("Datos de la bodega obtenidos correctamente.", data);
+      setBodegass(data);
+    } catch (err) {
+      console.error("Error al obtener los datos de la bodega:", err);
+    }
+  };
+
+
+
+  const fetchSolicitudes = async (fechaInicio, fechaFin, bodega) => {
+    try {
+
+
       const token = localStorage.getItem("token");
       const response = await axios.get(APIURL.getCreSolicitudCredito(), {
         headers: {
@@ -28,96 +112,214 @@ export function Dashboards() {
         params: {
           limit: 100,
           fechaInicio,
-          fechaFin
-        },
-      });
+          fechaFin,
+          bodega: selectedBodega === "todos" ? 0 : selectedBodega, // Si es "todos", no se envía el parámetro
+          estado: estadoFiltro === "todos" ? 0 : estadoFiltro,
 
+        },
+
+
+        
+      });
+  
       if (response.status === 200) {
-        const estadoCounts = { PENDIENTE: 0, APROBADO: 0, ANULADO: 0, RECHAZADO: 0 };
+        const estadoCounts = {
+          PENDIENTE: 0,
+          APROBADO: 0,
+          ANULADO: 0,
+          RECHAZADO: 0,
+        };
         const totalRecords = response.data.total;
-        setTotalSolicitudes(totalRecords); 
+        setTotalSolicitudes(totalRecords);
+  
+        const bodegaCounts = {}; // Aseguramos que siempre está definido
+        const vendedoresSet = new Set(); // Para contar vendedores únicos
 
         response.data.data.forEach((item) => {
           if (item.Estado === 1) estadoCounts.PENDIENTE++;
           else if (item.Estado === 2) estadoCounts.APROBADO++;
           else if (item.Estado === 3) estadoCounts.ANULADO++;
           else if (item.Estado === 4) estadoCounts.RECHAZADO++;
-        });
+  
 
+
+          if (item.idVendedor) {
+            vendedoresSet.add(item.idVendedor);
+          }
+        // Contamos las solicitudes por bodega
+        const bodegaId = item.Bodega; 
+        if (bodegaCounts[bodegaId]) {
+          bodegaCounts[bodegaId]++;
+        } else {
+          bodegaCounts[bodegaId] = 1;
+        }
+      });
+
+      setUniqueVendedores(vendedoresSet.size); // Guardamos la cantidad de vendedores únicos
+
+  
         setDoughnutData({
           labels: ["Pendiente", "Aprobado", "Anulado", "Rechazado"],
           datasets: [
             {
-              data: [estadoCounts.PENDIENTE, estadoCounts.APROBADO, estadoCounts.ANULADO, estadoCounts.RECHAZADO],
+              data: [
+                estadoCounts.PENDIENTE,
+                estadoCounts.APROBADO,
+                estadoCounts.ANULADO,
+                estadoCounts.RECHAZADO,
+              ],
               backgroundColor: ["#007bff", "#00c853", "#ffab00", "#ff3d00"],
             },
           ],
         });
+        const bodegaLabels = Object.keys(bodegaCounts).map((codigo) => {
+          const bodegaEncontrada = bodegas.find((b) => b.b_Bodega === Number(codigo));
+          return bodegaEncontrada ? bodegaEncontrada.b_Nombre : `Bodega ${codigo}`;
+        });
+
+
+      const bodegaValues = Object.values(bodegaCounts);
+
+
+      setBarData({
+        labels: bodegaLabels,
+        datasets: [
+          {
+            label: "Solicitudes por Bodega",
+            data: bodegaValues,
+            backgroundColor: "#007bff",
+          },
+        ],
+      });
       }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+  
+
+
+
+
+
+
 
   useEffect(() => {
     // Solo llamar a fetchSolicitudes si las fechas son válidas
-    if (fechaInicio && fechaFin) {
-      fetchSolicitudes(fechaInicio, fechaFin);
-    }
-  }, [fechaInicio, fechaFin]);
+      fetchSolicitudes(fechaInicio, fechaFin );
+    
+  }, [fechaInicio, fechaFin , selectedBodega , estadoFiltro] );
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
-
-      {/* Sección de filtros */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        <div>
-          <input
-            type="date"
-            className="p-2 w-full rounded border border-gray-300"
-            value={fechaInicio}
-            onChange={(e) => setFechaInicio(e.target.value)}
-          />
+{/* Sección de filtros */}
+      {/* Contenedor de los filtros y botón alineados en una sola fila */}
+      <div className="flex flex-col md:flex-row items-center gap-4">
+        
+        {/* Select de bodegas */}
+        <div className="w-full md:w-1/4">
+          <label htmlFor="bodega-select" className="block text-gray-700 font-semibold mb-1">
+            Bodega
+          </label>
+          <select
+            id="bodega-select"
+            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={selectedBodega}
+            onChange={handleBodegaChange}
+          >
+            <option value="todos">Todos</option>
+            {bodegas.length > 0 ? (
+              bodegas.map((bodega) => (
+                <option key={bodega.b_Bodega} value={bodega.b_Bodega}>
+                  {bodega.b_Nombre}
+                </option>
+              ))
+            ) : (
+              <option value="" disabled>No se encontraron bodegas.</option>
+            )}
+          </select>
         </div>
-        <div>
+
+        {/* Input Fecha Inicio */}
+        <div className="w-full md:w-1/4">
+          <label className="block text-gray-700 font-semibold mb-1">Fecha Inicio</label>
           <input
             type="date"
-            className="p-2 w-full rounded border border-gray-300"
+            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={fechaInicio}
+            onChange={(e) => {
+              const nuevaFechaInicio = e.target.value;
+    
+              // Validar que la fecha de inicio sea válida
+              if (isValidDate(nuevaFechaInicio)) {
+                setFechaInicio(nuevaFechaInicio);
+              } else {
+                // Si la fecha es inválida, se asigna la fecha de hoy
+                setFechaInicio(today);
+                alert("Fecha de inicio inválida. Se ha establecido la fecha de hoy.");
+              }
+            }}
+/>
+        </div>
+
+        {/* Input Fecha Fin */}
+        <div className="w-full md:w-1/4">
+
+
+          <label className="block text-gray-700 font-semibold mb-1">Fecha Fin</label>
+          <input
+            type="date"
+            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             value={fechaFin}
             onChange={(e) => setFechaFin(e.target.value)}
           />
         </div>
-        <div>
-          <button
-            onClick={() => fetchSolicitudes(fechaInicio, fechaFin)}
-            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+
+        <div className="w-full md:w-1/4">
+          <label className="block text-gray-700 font-semibold mb-1">Estado</label>
+          <select
+            className="w-full p-2 border-2 border-gray-300 rounded-md"
+            value={estadoFiltro}
+            onChange={(e) => setEstadoFiltro(e.target.value)}
           >
-            Filtrar
-          </button>
+            {estadosOpciones.map((estado) => (
+              <option key={estado.value} value={estado.value}>
+                {estado.label}
+              </option>
+            ))}
+          </select>
         </div>
+
+
+    
+
       </div>
 
       {/* Sección de tarjetas */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 bt-6 mt-6">
         <div className="bg-white p-4 rounded shadow-md">
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-sm text-gray-500">Número de solicitudes de crédito</p>
+              <p className="text-sm text-gray-500">
+                Número de solicitudes de crédito
+              </p>
               <h2 className="text-xl font-semibold">{totalSolicitudes}</h2>
-              <p className="text-green-500">+4.4%</p>
             </div>
-            <div className="text-4xl text-blue-600"><MonetizationOn /></div>
+            <div className="text-4xl text-blue-600">
+              <MonetizationOn />
+            </div>
           </div>
         </div>
         <div className="bg-white p-4 rounded shadow-md">
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-sm text-gray-500">USERS</p>
-              <h2 className="text-xl font-semibold">50,021</h2>
-              <p className="text-green-500">+2.6%</p>
+              <p className="text-sm text-gray-500">Vendedores involucrados </p>
+              <h2 className="text-xl font-semibold">{uniqueVendedores}</h2>
             </div>
-            <div className="text-4xl text-green-500"><People /></div>
+            <div className="text-4xl text-green-500">
+              <People />
+            </div>
           </div>
         </div>
         <div className="bg-white p-4 rounded shadow-md">
@@ -127,7 +329,9 @@ export function Dashboards() {
               <h2 className="text-xl font-semibold">45,021</h2>
               <p className="text-green-500">+3.1%</p>
             </div>
-            <div className="text-4xl text-orange-500"><ShoppingCart /></div>
+            <div className="text-4xl text-orange-500">
+              <ShoppingCart />
+            </div>
           </div>
         </div>
         <div className="bg-white p-4 rounded shadow-md">
@@ -137,7 +341,9 @@ export function Dashboards() {
               <h2 className="text-xl font-semibold">20,516</h2>
               <p className="text-green-500">+3.1%</p>
             </div>
-            <div className="text-4xl text-red-500"><ConfirmationNumber /></div>
+            <div className="text-4xl text-red-500">
+              <ConfirmationNumber />
+            </div>
           </div>
         </div>
       </div>
@@ -146,7 +352,9 @@ export function Dashboards() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
         <div className="bg-white p-4 rounded shadow-md">
           <h3 className="text-xl font-semibold mb-4">Bar Chart</h3>
-          <Bar data={{ labels: ["Jan", "Feb", "Mar"], datasets: [{ label: "Sales", data: [10, 20, 30], backgroundColor: "#007bff" }] }} />
+          
+              <Bar data={barData} />
+          
         </div>
         <div className="bg-white p-4 rounded shadow-md">
           <h3 className="text-xl font-semibold mb-4">Doughnut Chart</h3>
