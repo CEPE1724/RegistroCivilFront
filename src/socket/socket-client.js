1f18e2c2cc171e0b541d7ddcf7a3784f85da95fe
CEPE1724 <edisonnacional1@hotmail.com>
2025-06-18 11:35:46 -0500

    Actualizacion dle gitignore

--- src/socket/socket-client.js
+++ src/socket/socket-client.js
@@ -3,8 +3,8 @@ import { Manager, Socket } from "socket.io-client";
 let socket;
 export const connectToServer = (token) => {
 //https://backregistrocivil.appservices.com.ec/socket.io/socket.io.js
-  //const manager = new Manager("https://backregistrocivil.appservices.com.ec/socket.io/socket.io.js",
-  const manager = new Manager("http://192.168.2.67:3025",
+  const manager = new Manager("https://backregistrocivil.appservices.com.ec/socket.io/socket.io.js",
+  //const manager = new Manager("http://192.168.2.67:3025",
 
     {
       extraHeaders: {
