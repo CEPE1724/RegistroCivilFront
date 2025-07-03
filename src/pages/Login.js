2a5cd9f6bad52b9e40eefa827977e582ca1d77d2
CEPE1724 <edisonnacional1@gmail.com>
2025-07-03 11:34:57 -0500

    actualizacion loginb

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -355,7 +355,7 @@ const Login = () => {
                 <CancelIcon sx={{ color: "#f44336", fontSize: "1rem" }} />
               )}
               <Typography variant="body2" sx={{ color: passwordValidation.minLength ? "#4caf50" : "#f44336" }}>
-                Mínimo 8 caracteres
+                Mínimo 8 caracteres y máximo 12 caracteres
               </Typography>
             </Box>
 
