26602f6c8d98f7f6de4658e41e49eba29437ce99
DanielPoint <dnavarrete@point.com.ec>
2025-07-09 09:03:21 -0500

    Bloquear ckeck domicilio cuando se preaprueba una solicitud y croquis para informe del motorizado

--- src/configApi/apiConfig.js
+++ src/configApi/apiConfig.js
@@ -178,9 +178,8 @@ export const APIURL = {
   // Cambiar contraseña del usuario
   cambiarClave: () => `${API_BASE_URL}usuarios/cambiar-clave`,
   recuperarClave: () => `${API_BASE_URL}usuarios/recuperar-clave`, 
-
-
   patch_ClientesVerifTerren: (id) => `${API_BASE_URL}clientes-verificacion-terrena/update/${id}`,
-  get_ClientesVerifTerrenporId: (id) => `${API_BASE_URL}clientes-verificacion-terrena/allbyID/${id}` 
+  get_ClientesVerifTerrenporId: (id) => `${API_BASE_URL}clientes-verificacion-terrena/allbyID/${id}`,
+  get_CoordenadasInforme: (id) => `${API_BASE_URL}clientes-verificacion-terrena/coordInforme/${id}`,
 
 };
