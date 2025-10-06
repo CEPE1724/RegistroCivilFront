e25952f111f77e386840d7383d1d4cd1b8889405
CEPE1724 <edisonnacional1@hotmail.com>
2025-10-06 16:56:00 -0500

    prueba de automatizacion

--- src/pages/Desayuno.js
+++ src/pages/Desayuno.js
@@ -64,7 +64,7 @@ const Desayuno = () => {
                         alt="Logo Point"
                         className="w-32 h-auto mx-auto mb-6 animate-pulse"
                     />
-                    <h1 className="text-3xl font-bold text-gray-800 mb-4">de ciim erddp </h1>
+                    <h1 className="text-3xl font-bold text-gray-800 mb-4">prueba de automatizacion </h1>
                     <p className="text-gray-600 text-lg mb-6">
                         Acompáñanos a un evento especial organizado por el equipo directivo.
                     </p>
